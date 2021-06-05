09df30f2f97e03793ad7e23db965bb7ae908d720
pranav <vpranavkrishna@gmail.com>
2021-06-05 12:27:59 +0530

    First Commit

---
+++ app/src/main/java/com/spider_inductions/spider_task_1/MainActivity.java
@@ -0,0 +1,15 @@
+
+package com.spider_inductions.spider_task_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
